52cbe6ebdf501c8848bc8f2d72b6828531733bc1
Insamedkv <42409721+Insamedkv@users.noreply.github.com>
2021-02-18 15:31:41 +0300

    refactor: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    if (Math.sign(n) == -1) {
+        n = n * -1;
+    }
+    let i = 0;
+    while (n > 0) {
+        i = i * 10 + n % 10;
+        n = Math.floor((n / 10));
+      }
+    return i
 }
